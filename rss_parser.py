6799cccd370738b6404e2da9242127db85599092
Evgeny Kostryukov <kostrukov.evgeny2018@yandex.ru>
2024-06-04 15:49:01 +0300

    Changed company counter and other minor fixes

--- rss_parser.py
+++ rss_parser.py
@@ -1,69 +1,91 @@
+import os
 import httpx
+import hashlib
 import asyncio
 import feedparser
-import re
-import os
-import hashlib
-from collections import deque
 from newspaper import Article
 
 
+output_dir_for_news = "news"
+mentions_file = "mentions.txt"
+companies_file = "companies.txt"
 
-def load_companies(file_path, encoding='utf-8'):
-    with open(file_path, "r", encoding=encoding) as file:
-        companies = file.read().splitlines()
+
+def load_companies_from_file(companies_file):
+    """Считывает список компаний из файла."""
+    if not os.path.isfile(companies_file):
+        print(f"Companies file {companies_file} not found.")
+        return []
+
+    with open(companies_file, "r", encoding='utf-8') as file:
+        companies = [line.strip() for line in file if line.strip()]
+
     return companies
 
 
-def find_russian_companies(text, companies_file):
-
-    companies = load_companies(companies_file)
-
-    pattern = r'[А-ЯЁ][а-яё]+(?:\s+[А-ЯЁ][а-яё]+)*'
-    matches = re.findall(pattern, text)
-
-    relevant_companies = [match for match in matches if match in companies]
-
-    return relevant_companies
-
 async def fetch_article(httpx_client, url):
+    """Извлекает полный текст статьи по URL."""
     try:
         response = await httpx_client.get(url)
         response.raise_for_status()
         article = Article(url)
         article.download()
         article.parse()
-
-        # Получаем текст статьи
-        article_text = article.text
-
-        # Разбиваем текст на предложения
-        sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', article_text)
-
-        # Оставляем только первое предложение и удаляем абзацы
-        first_sentence = sentences[0]
-        remaining_text = ' '.join(sentences[1:]) if len(sentences) > 1 else ''
-
-        # Склеиваем первое предложение и оставшуюся часть текста
-        article_text_processed = f"{first_sentence}. {remaining_text}"
-
-        return article_text_processed
+        return article.text
+    except httpx.RequestError as e:
+        print(f"Request error fetching RSS feed: {e}")
+        return ''
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error fetching RSS feed: {e}")
+        return ''
     except Exception as e:
-        print(f"Error fetching article: {e}")
+        print(f"Unknown error fetching RSS feed: {e}")
         return ''
 
-async def rss_parser(httpx_client, posted_q, output_dir, rss_links, companies_file):
-    os.makedirs(output_dir, exist_ok=True)
 
-    print("Starting to parse RSS feeds")
+def update_mentions_file(company):
+    """Обновляет файл с упоминаниями компаний."""
+    mentions = {}
+
+    # Чтение текущих упоминаний из файла
+    if os.path.isfile(mentions_file):
+        with open(mentions_file, "r", encoding='utf-8') as file:
+            for line in file:
+                name, count = line.strip().split(": ")
+                mentions[name] = int(count)
+
+    # Обновление счетчика упоминаний
+    if company in mentions:
+        mentions[company] += 1
+    else:
+        mentions[company] = 1
+
+    # Запись обновленных упоминаний обратно в файл
+    with open(mentions_file, "w", encoding='utf-8') as file:
+        for name, count in mentions.items():
+            file.write(f"{name}: {count}\n")
+
+
+async def rss_parser(httpx_client, rss_links):
+    """Парсит RSS-ленты и сохраняет новости"""
+    os.makedirs(output_dir_for_news, exist_ok=True)
+    companies = load_companies_from_file(companies_file)
+
     while True:
+        print("Starting to parse RSS feeds")
+        count_parced_news = 0
         for rss_link in rss_links:
             try:
                 response = await httpx_client.get(rss_link)
                 response.raise_for_status()
+            except httpx.RequestError as e:
+                print(f"Request error fetching RSS feed: {e}")
+                continue
+            except httpx.HTTPStatusError as e:
+                print(f"HTTP error fetching RSS feed: {e}")
+                continue
             except Exception as e:
-                print(f"Error fetching RSS feed: {e}")
-                await asyncio.sleep(10)
+                print(f"Unknown error fetching RSS feed: {e}")
                 continue
 
             feed = feedparser.parse(response.text)
@@ -75,31 +97,34 @@ async def rss_parser(httpx_client, posted_q, output_dir, rss_links, companies_fi
                 if not link:
                     continue
 
+                news_hash = hashlib.sha256(title.encode('utf-8')).hexdigest()
+                if os.path.isfile(os.path.join(output_dir_for_news, f"{news_hash}.txt")):
+                    continue
+
                 full_text = await fetch_article(httpx_client, link)
                 if not full_text:
                     continue
 
                 news_text = f'{title}\n{full_text}'
 
-                news_hash = hashlib.sha256(title.encode('utf-8')).hexdigest()
-                if news_hash in posted_q:
-                    continue
-
+                # Генерация уникального имени файла для спаршенной новости
                 filename = f"{news_hash}.txt"
-                filepath = os.path.join(output_dir, filename)
-                print(f"\nSaving news: {title}\nlink: {link}")
-
+                filepath = os.path.join(output_dir_for_news, filename)
                 with open(filepath, "w", encoding='utf-8') as file:
                     file.write(news_text)
 
-                companies = find_russian_companies(news_text, companies_file)
-                if companies:
-                    company_count = {company: companies.count(company) for company in set(companies)}
-                    with open(os.path.join(output_dir, f"{news_hash}_companies.txt"), "w", encoding='utf-8') as file:
-                        for company, count in company_count.items():
-                            file.write(f"{company}: {count}\n")
+                # Проверка на упоминание компаний
+                for company in companies:
+                    if company in news_text:
+                        update_mentions_file(company)
 
-                posted_q.appendleft(news_hash)
+                count_parced_news += 1
+                print(f"\n{count_parced_news}. Saving news: {title}\nlink: {link}")
+
+        # Пятиминутное ожидание перед следующим циклом
+        print(f"\n{count_parced_news} news from sources have been parced. Waiting for 5 minutes before next parsing...")
+        await asyncio.sleep(300)
+
 
 if __name__ == "__main__":
     rss_links = [
@@ -111,14 +136,7 @@ if __name__ == "__main__":
         "https://ru.investing.com/rss/bonds_Government.rss",
         "https://ru.investing.com/rss/bonds_Corporate.rss",
         "https://ru.investing.com/rss/news_301.rss"
-    ]
-
-    posted_q = deque(maxlen=60)
-
-    output_dir = "news"
-
-    companies_file = "companies.txt"
-
+        ]
 
     httpx_client = httpx.AsyncClient()
-    asyncio.run(rss_parser(httpx_client, posted_q, output_dir, rss_links, companies_file))
+    asyncio.run(rss_parser(httpx_client, rss_links))
